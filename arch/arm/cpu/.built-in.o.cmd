2c07cd71b7f3be7cc4b7c5e0dae664d13029aaf0
collinwu <collin.wu@bematech.com.tw>
2017-10-11 14:31:24 +0800

    Solve compilation problem by add files at arch/arm

---
+++ arch/arm/cpu/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_arch/arm/cpu/built-in.o :=  rm -f arch/arm/cpu/built-in.o; /home/ennis/project/imx6slevk/fsl-linaro-toolchain-master/bin/arm-fsl-linux-gnueabi-ar rcs arch/arm/cpu/built-in.o
---
+++ arch/arm/lib/.lib.a.cmd
@@ -0,0 +1,1 @@
+cmd_arch/arm/lib/lib.a := rm -f arch/arm/lib/lib.a; /home/ennis/project/imx6slevk/fsl-linaro-toolchain-master/bin/arm-fsl-linux-gnueabi-ar rcs arch/arm/lib/lib.a 
