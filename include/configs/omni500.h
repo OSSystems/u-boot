b1690fc4fa56a178ae7b147728df9689ff5cfd8d
Fabio Estevam <festevam@gmail.com>
2023-04-13 21:38:51 -0300

    omni500: Add initial support
    Add the initial support for Jacto's omni500 board.
    
    This board uses a Toradex Colibri.MX66DL SoM and a baseboard
    designed by Jacto.
    
    Signed-off-by: Fabio Estevam <festevam@gmail.com>
    Signed-off-by: Luan Rafael Carneiro <luan.rafael@ossystems.com.br>

---
+++ include/configs/omni500.h
@@ -0,0 +1,60 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2013-2019 Toradex, Inc.
+ *
+ * Configuration settings for the Toradex Colibri iMX6
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include <linux/stringify.h>
+
+#include "mx6_common.h"
+
+#include <asm/arch/imx-regs.h>
+#include <asm/mach-imx/gpio.h>
+
+#define CFG_MXC_UART_BASE		UART1_BASE
+
+/* MMC Configs */
+#define CFG_SYS_FSL_ESDHC_ADDR	0
+#define CFG_SYS_FSL_USDHC_NUM	2
+
+/* USB Configs */
+/* Host */
+#define CFG_MXC_USB_PORTSC		(PORT_PTS_UTMI | PORT_PTS_PTW)
+#define CFG_MXC_USB_FLAGS		0
+
+/* Command definition */
+
+#define BOOT_TARGET_DEVICES(func) \
+	func(MMC, mmc, 0) \
+	func(USB, usb, 0)
+
+#include <config_distro_bootcmd.h>
+
+#define MEM_LAYOUT_ENV_SETTINGS \
+	"bootm_size=0x10000000\0" \
+	"fdt_addr_r=0x18200000\0" \
+	"kernel_addr_r=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
+	"pxefile_addr_r=0x18300000\0" \
+	"ramdisk_addr_r=0x18400000\0" \
+	"scriptaddr=0x18280000\0"
+
+#define CFG_EXTRA_ENV_SETTINGS \
+	BOOTENV \
+	"boot_file=zImage\0" \
+	"console=ttymxc0\0" \
+	MEM_LAYOUT_ENV_SETTINGS \
+
+/* Miscellaneous configurable options */
+
+/* Physical Memory Map */
+#define PHYS_SDRAM			MMDC0_ARB_BASE_ADDR
+
+#define CFG_SYS_SDRAM_BASE		PHYS_SDRAM
+#define CFG_SYS_INIT_RAM_ADDR	IRAM_BASE_ADDR
+#define CFG_SYS_INIT_RAM_SIZE	IRAM_SIZE
+
+#endif	/* __CONFIG_H */
